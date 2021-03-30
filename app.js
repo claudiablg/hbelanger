dbaaabb7aeb0430f48ba99585981bebd08bc14e1
claudiablg <79177492+claudiablg@users.noreply.github.com>
2021-03-30 17:24:02 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const navSlide = () => {
+
+    const hamburger = document.querySelector('.menu-btn');
+    const nav = document.querySelector('.main-menu');
+
+    hamburger.addEventListener('click', () => {
+         //Toggle Nav
+        nav.classList.toggle('nav-active');
+         //Hamburger Animation 
+        hamburger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
+
+//Carousel
+
+$(".gallery").slick({
+    arrows: true,
+    dots: false,
+    centerMode: true,
+    variableWidth: true,
+    adaptiveHeight: false,
+    prevArrow: $('.gallery-arrow.prev'),
+    nextArrow: $('.gallery-arrow.next')
+});
