f530803ed04e942cf31b1e5ffda660e5964e619b
claudiablg <79177492+claudiablg@users.noreply.github.com>
2021-03-30 23:34:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
+$(function() {
 
-const navSlide = () => {
+    const navSlide = () => {
+        const hamburger = document.querySelector('.menu-btn');
+        const nav = document.querySelector('.main-menu');
 
-    const hamburger = document.querySelector('.menu-btn');
-    const nav = document.querySelector('.main-menu');
+        hamburger.addEventListener('click', () => {
+            //Toggle Nav
+            nav.classList.toggle('nav-active');
+            //Hamburger Animation 
+            hamburger.classList.toggle('toggle');
+        });
+    }
+    navSlide();
 
-    hamburger.addEventListener('click', () => {
-         //Toggle Nav
-        nav.classList.toggle('nav-active');
-         //Hamburger Animation 
-        hamburger.classList.toggle('toggle');
+    targetElement.ontouchend = (e) => {
+        e.preventDefault();
+    };
+
+    document.body.style.overflow = 'hidden';
+
+    //Carousel
+
+    $(".gallery").slick({
+        arrows: true,
+        dots: false,
+        centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: false,
+        prevArrow: $('.gallery-arrow.prev'),
+        nextArrow: $('.gallery-arrow.next')
     });
-
-}
-
-targetElement.ontouchend = (e) => {
-    e.preventDefault();
-};
-
-document.body.style.overflow = 'hidden';
-
-navSlide();
-
-//Carousel
-
-$(".gallery").slick({
-    arrows: true,
-    dots: false,
-    centerMode: true,
-    variableWidth: true,
-    adaptiveHeight: false,
-    prevArrow: $('.gallery-arrow.prev'),
-    nextArrow: $('.gallery-arrow.next')
 });
